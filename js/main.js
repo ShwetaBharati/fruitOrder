bd968e1db3dc19d87d540d38fdbb0cf7bcfcdf97
Shweta Bharati <109725870+ShwetaBharati@users.noreply.github.com>
2023-08-19 01:02:14 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,168 @@
+/******************************************************************
+  Template Name: FoodOrder
+  Description:  FoodOrder eCommerce  HTML, CSS, JavaScript Template
+  Author: Shweta
+  Author Email: shwetabharati888@gmail.com
+******************************************************************/
+
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Product filter
+        --------------------*/
+        $('.featured-controls li').on('click', function () {
+            $('.featured-controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.featured-filter').length > 0) {
+            var containerEl = document.querySelector('.featured-filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    /*--------------------------------------
+        Mobile Section Menu Open and Close
+    ----------------------------------------*/
+    $('.menu-categories-all').on('click', function(){
+        $('.menu-categories ul').slideToggle(400);
+    });
+
+    /*-----------------------
+        Categories Slider
+    ------------------------*/
+    $(".categories-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+
+            0: {
+                items: 1,
+            },
+
+            480: {
+                items: 2,
+            },
+
+            768: {
+                items: 3,
+            },
+
+            992: {
+                items: 4,
+            }
+        }
+    });
+
+    /*--------------------------
+       Top Product Slider
+    ----------------------------*/
+    $(".top-product-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*-----------------------------
+        Product Discount Slider
+    -------------------------------*/
+    $(".product-discount-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+
+            320: {
+                items: 1,
+            },
+
+            480: {
+                items: 2,
+            },
+
+            768: {
+                items: 2,
+            },
+
+            992: {
+                items: 3,
+            }
+        }
+    });
+
+    /*---------------------------------
+        Product Details Pic Slider
+    ----------------------------------*/
+    $(".product-details-pic-slider").owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 4,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*-----------------------
+		Price Range Slider
+	------------------------ */
+    var rangeSlider = $(".price-range"),
+        minamount = $("#minamount"),
+        maxamount = $("#maxamount"),
+        minPrice = rangeSlider.data('min'),
+        maxPrice = rangeSlider.data('max');
+        rangeSlider.slider({
+        range: true,
+        min: minPrice,
+        max: maxPrice,
+        values: [minPrice, maxPrice],
+        slide: function (event, ui) {
+            minamount.val('RS' + ui.values[0]);
+            maxamount.val('RS' + ui.values[1]);
+        }
+    });
+    minamount.val('RS' + rangeSlider.slider("values", 0));
+    maxamount.val('RS' + rangeSlider.slider("values", 1));
+
+    /*--------------------------
+        Select
+    ----------------------------*/
+    $("select").niceSelect();
+
+})(jQuery);
